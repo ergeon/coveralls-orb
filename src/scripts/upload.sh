699a7e99f50c956814f7ba9b25dddcf2d56dcca1
Chapayev Dauren <chapaevdauren@gmail.com>
2022-08-12 18:14:06 +0600

    [ENG-16770] Add skip coverage check
    Skip the coverage if commit message contains [skip cov] text

--- src/scripts/upload.sh
+++ src/scripts/upload.sh
@@ -28,37 +28,52 @@ if [ "$PARALLEL" = true ]; then
   export COVERALLS_PARALLEL=true
 fi
 
-if [ -n "$CIRCLE_PULL_REQUEST" ] || [ "${CIRCLE_BRANCH}" = "develop" ] || [ "${CIRCLE_BRANCH}" = "master" ]; then
-  echo "Uploading coverage report to Coveralls..."
-  echo "Pull request number: $CIRCLE_PULL_REQUEST"
-  if [ ! -r "$PATH_TO_LCOV" ]; then
-    CI_PULL_REQUEST=$CIRCLE_PULL_REQUEST coveralls
+SKIP_COVERAGE_TAG='[skip cov]'
+
+# Get last commit message
+readonly LAST_COMMIT_LOG=$(git log -1 --pretty=format:"%s")
+echo "Last commit log: $LAST_COMMIT_LOG"
+
+readonly FILTER_COUNT=$(echo "$LAST_COMMIT_LOG" | grep -c "${SKIP_COVERAGE_TAG}" )
+echo "Number of occurrence of '${SKIP_COVERAGE_TAG}' in '${LAST_COMMIT_LOG}': ${FILTER_COUNT}"
+
+if [[ "$FILTER_COUNT" -eq 0 ]]; then
+  echo "all good, continue"
+  if [ -n "$CIRCLE_PULL_REQUEST" ] || [ "${CIRCLE_BRANCH}" = "develop" ] || [ "${CIRCLE_BRANCH}" = "master" ]; then
+    echo "Uploading coverage report to Coveralls..."
+    echo "Pull request number: $CIRCLE_PULL_REQUEST"
+    if [ ! -r "$PATH_TO_LCOV" ]; then
+      CI_PULL_REQUEST=$CIRCLE_PULL_REQUEST coveralls
+    else
+      < "$PATH_TO_LCOV" CI_PULL_REQUEST=$CIRCLE_PULL_REQUEST coveralls
+    fi
   else
-    < "$PATH_TO_LCOV" CI_PULL_REQUEST=$CIRCLE_PULL_REQUEST coveralls
+    for I in 1 2 3 4 5
+    do
+      PULL_REQUEST_NUMBER=$(curl -sb -H "Accept: application/json" -H "Authorization: Token $GITHUB_API_TOKEN" "https://api.github.com/repos/$ORG_NAME/$CIRCLE_PROJECT_REPONAME/commits/$CIRCLE_SHA1/pulls" | python -c 'import json,sys;data=json.load(sys.stdin);pr_number=data[0]["number"] if data and isinstance(data, list) else "";print(pr_number)')
+      echo "Pull request number: $PULL_REQUEST_NUMBER"
+      if [ -n "$PULL_REQUEST_NUMBER" ]; then
+        if [ ! -r "$PATH_TO_LCOV" ]; then
+          CI_PULL_REQUEST=$PULL_REQUEST_NUMBER coveralls
+        else
+          PULL_REQUEST_URL="https://github.com/ergeon/$CIRCLE_PROJECT_REPONAME/pull/$PULL_REQUEST_NUMBER"
+          echo "$PULL_REQUEST_URL"
+          < "$PATH_TO_LCOV" CI_PULL_REQUEST=$PULL_REQUEST_URL coveralls
+        fi
+        break
+      else
+        if [[ $I == 5 ]]; then
+          echo "Pull Request wasn't found for this commit."
+          echo "Please re-run this build once Pull Request will be opened."
+          exit 1
+        fi
+        echo "PR not found yet, trying again after a while."
+        echo "Waiting for 60 seconds..."
+        sleep 60
+      fi
+    done
   fi
 else
-  for I in 1 2 3 4 5
-  do
-    PULL_REQUEST_NUMBER=$(curl -sb -H "Accept: application/json" -H "Authorization: Token $GITHUB_API_TOKEN" "https://api.github.com/repos/$ORG_NAME/$CIRCLE_PROJECT_REPONAME/commits/$CIRCLE_SHA1/pulls" | python -c 'import json,sys;data=json.load(sys.stdin);pr_number=data[0]["number"] if data and isinstance(data, list) else "";print(pr_number)')
-    echo "Pull request number: $PULL_REQUEST_NUMBER"
-    if [ -n "$PULL_REQUEST_NUMBER" ]; then
-      if [ ! -r "$PATH_TO_LCOV" ]; then
-        CI_PULL_REQUEST=$PULL_REQUEST_NUMBER coveralls
-      else
-        PULL_REQUEST_URL="https://github.com/ergeon/$CIRCLE_PROJECT_REPONAME/pull/$PULL_REQUEST_NUMBER"
-        echo "$PULL_REQUEST_URL"
-        < "$PATH_TO_LCOV" CI_PULL_REQUEST=$PULL_REQUEST_URL coveralls
-      fi
-      break
-    else
-      if [[ $I == 5 ]]; then
-        echo "Pull Request wasn't found for this commit."
-        echo "Please re-run this build once Pull Request will be opened."
-        exit 1
-      fi
-      echo "PR not found yet, trying again after a while."
-      echo "Waiting for 60 seconds..."
-      sleep 60
-    fi
-  done
+  echo "the last commit log '${LAST_COMMIT_LOG}' contains '${SKIP_COVERAGE_TAG}', stopping"
+  curl -X POST -H "Accept: application/vnd.github+json" -H "Authorization: token $GITHUB_API_TOKEN" "https://api.github.com/repos/ergeon/$CIRCLE_PROJECT_REPONAME/statuses/$CIRCLE_SHA1" -d '{"state":"success","description":"Coverage passed!","context":"coverage/coveralls"}'
 fi
